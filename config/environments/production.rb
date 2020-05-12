b324cb00f89797999a56b7751d938dd69971d58a
Adel MALIK-MONROCQ <dedeloum@icloud.com>
2020-05-13 00:33:54 +0200

    beta

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # mailer
+  config.action_mailer.default_url_options = { :host => 'thp-w6-eventbrite.herokuapp.com'}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   get '/', to: 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
