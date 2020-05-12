9bcf07869297062212bd8811da04497aef828e27
Adel MALIK-MONROCQ <dedeloum@icloud.com>
2020-05-12 18:31:48 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create()
+user2 = User.create()
+admin = User.create()
+event = Event.create(user: admin, start_date: DateTime.now+1.day, duration: 50, title: "Testo", description: "Ceci est un test ne cherche pas à savoir mon frère", price: 50, location: "SM")
+Attendance.create(user: user1, event: event)
+Attendance.create(user: user2, event: event)
